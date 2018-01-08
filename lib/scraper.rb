e81edb26eb40b31213a2db30f4b162131c3e1b38
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2018-01-08 20:34:07 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = Nokogiri::HTML(open(index_url))
+    html = Nokogiri::HTML(File.read(index_url))
 
     binding.pry
   end
